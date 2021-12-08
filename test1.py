be4bca3a81070335dc616b1a0dd8da35be6ba3d3
Octavio Serrano <octavioserrano@Octavio-Serranos-MacBook-Pro-10.local>
2021-12-08 14:41:28 -0800

    do over

---
+++ test1.py
@@ -0,0 +1,41 @@
+from fractions import Fraction 
+
+import unittest 
+
+
+
+from my_sum import sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """"
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fractions(self):
+        """"
+        Test that it can sum a list of fractions
+        """ 
+        data = [Fraction(1,4), Fraction(1,4), Fraction(2,5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+    
+# how to call on command line -v = verbose 
+# python -m unittest test1 
+# python3 -m unittest -v test1
+# python3 -m unittest discover
+
+# when multiple test files, as long as the test*.py naming pattern
+# you can provide the name of the directory instead by using the -s flag
+# and the name of the directory 
+#python3 -m unittest discover -s tests
+ 
+#python3 -m unittest discover -s test1 -t src
